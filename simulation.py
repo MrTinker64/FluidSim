b4d1194a4140b21a42be9c827644321a3a901c64
MrTinker64 <avi.micah@gmail.com>
2023-10-30 17:21:21 -0700

    finished advection

--- simulation.py
+++ simulation.py
@@ -15,6 +15,9 @@ h = h_x + h_y / 2
 num_x = math.floor(width / h)
 num_y = math.floor(height / h)
 
+U_FIELD = 0
+V_FIELD = 1
+
 grid = np.zeros((height, width, 2))
 
 u_velocity = np.zeros((height, width + 1))  # horizontal velocity, located on the vertical faces of the cells
@@ -66,22 +69,77 @@ def projection():
             v_velocity[i,j+1] -= div * sy1
             
             grid[i,j,1] += div / s * (grid[i,j,2] * h) / dt
+            
+def sample_field(x, y, field):
+    n = num_y
+    h1 = 1.0 / h
+    h2 = 0.5 * h
 
-def avgU(i, j):
-    u = (u_velocity[i,j-1] + u_velocity[i,j] + u_velocity[i,j-1] + u_velocity[i+1,j]) / 4
-    return u
+    x = max(min(x, num_x * h), h)
+    y = max(min(y, num_y * h), h)
 
-def avgV(i, j):
-    v = (v_velocity[i,j-1] + v_velocity[i,j] + v_velocity[i,j-1] + v_velocity[i+1,j]) / 4
-    return v
+    dx = 0.0
+    dy = 0.0
+
+    if field == U_FIELD:
+        f = u_velocity
+        dy = h2
+    elif field == V_FIELD:
+        f = v_velocity
+        dx = h2
+
+    x0 = min(int((x - dx) * h1), num_x - 1)
+    tx = ((x - dx) - x0 * h) * h1
+    x1 = min(x0 + 1, num_x - 1)
+
+    y0 = min(int((y - dy) * h1), num_y - 1)
+    ty = ((y - dy) - y0 * h) * h1
+    y1 = min(y0 + 1, num_y - 1)
+
+    sx = 1.0 - tx
+    sy = 1.0 - ty
+
+    val = sx * sy * f[x0 * n + y0] + \
+        tx * sy * f[x1 * n + y0] + \
+        tx * ty * f[x1 * n + y1] + \
+        sx * ty * f[x0 * n + y1]
+
+    return val
 
 def advectVel():
-    newU = u_velocity
-    newV = v_velocity
+    new_u = u_velocity
+    new_v = v_velocity
     
     for i in range(height):
-        for j in range(width):
-             pass
+        for j in range(width + 1):
+    # Compute the full velocity value at the point
+        # figure out the vertical component v bar by averaging surrounding v values
+            x = i * h
+            y = j * h + h/2
+            u = u_velocity[i,j]
+            v = (v_velocity[i,j], v_velocity[i,j+1], v_velocity[i-1,j], v_velocity[i-1,j+1]) / 4
+            x = x - dt*u
+            y = y - dt*v
+            
+            u = sample_field(x, y, U_FIELD)
+            
+            new_u[i,j] = u
+            
+    for i in range(height):
+        for j in range(width + 1):
+            x = i * h + h/2
+            y = j * h
+            u = (u_velocity[i,j], u_velocity[i,j+1], u_velocity[i-1,j], u_velocity[i-1,j+1]) / 4
+            v = v_velocity[i,j]
+            x = x - dt*u
+            y = y - dt*v
+            
+            v = sample_field(x, y, V_FIELD)
+            
+            new_v[i,j] = v
+            
+    u_velocity = new_u
+    v_velocity = new_v
 
 while current_simulation_time < total_simulation_time:        
     start_real_time = time.time() # Capture the real-world time before processing    
